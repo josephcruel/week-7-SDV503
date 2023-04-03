84abf4fc5e8208bd2329b058d9e68d3e29b9d65a
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-04-03 12:07:00 +1200

    added comments to code

--- src/JSFundementals.js
+++ src/JSFundementals.js
@@ -1,9 +1,11 @@
 //Create a new function that takes an array with only numbers and return the first element. 
 let x = 5;
 console.log("Hello Class!")
+//A function that give the first value of an array
 function firstElementValue(arr) {
     return arr[0][0]
 }
+//prints true if condition is met.
 if(x == 5) {
     console.log("True")
 }
