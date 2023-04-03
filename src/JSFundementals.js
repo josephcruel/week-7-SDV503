3b97cdd6f0368770faf28cae2aa4c46c2440635e
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-04-03 12:20:18 +1200

    more functions

--- src/JSFundementals.js
+++ src/JSFundementals.js
@@ -1,14 +1,41 @@
-//Create a new function that takes an array with only numbers and return the first element. 
-let x = 5;
-console.log("Hello Class!")
-//A function that give the first value of an array
-function firstElementValue(arr) {
-    return arr[0][0]
-}
-//prints true if condition is met.
-if(x == 5) {
-    console.log("True")
-}
+// //Create a new function that takes an array with only numbers and return the first element. 
+// let x = 5;
+// console.log("Hello Class!")
+// //A function that give the first value of an array
+// function firstElementValue(arr) {
+//     return arr[0][0]
+// }
+// //prints true if condition is met.
+// if(x == 5) {
+//     console.log("True")
+// }
 
-//Calling the function to return the first element of the array.
-console.log(firstElementValue([["Joseph"],[1, 2, 3, 4, 5 ,6]]))
+// //Calling the function to return the first element of the array.
+// console.log(firstElementValue([["Joseph"],[1, 2, 3, 4, 5 ,6]]))
+
+
+
+
+// //Create a function that takes a number, increment the number by 1 and return the result.
+// //Function that give add an original number by "1".
+// function incNum(x) {
+//   return  x += 1
+    
+// }
+// //Invoking the function
+// console.log(incNum(10))
+
+
+// //Create a function that takes an int minutes and convert it in seconds.
+
+// function int(x) {
+//     return int = (x * 60)
+// }
+// console.log(int(3))
+
+// //second way of doing it
+
+// function conSec(min) {
+//     return (min * 60)
+// }
+// console.log(conSec(3))
