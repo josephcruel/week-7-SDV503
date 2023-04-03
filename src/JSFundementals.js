69b00c78182d58202a51ce35741d8004e39ee5d4
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-04-03 12:04:39 +1200

    Calling the function for the first element of an array

---
+++ src/JSFundementals.js
@@ -0,0 +1,12 @@
+//Create a new function that takes an array with only numbers and return the first element. 
+let x = 5;
+console.log("Hello Class!")
+function firstElementValue(arr) {
+    return arr[0][0]
+}
+if(x == 5) {
+    console.log("True")
+}
+
+//Calling the function to return the first element of the array.
+console.log(firstElementValue([["Joseph"],[1, 2, 3, 4, 5 ,6]]))
